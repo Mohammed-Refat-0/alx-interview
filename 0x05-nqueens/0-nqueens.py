be3c595d59533e0a85601980eebfcab9de72de54
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-02 00:42:30 +0300

    ans file

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''N queens problem module'''
+import sys
+
+
+def solve(row, column):
+    solver = [[]]
+    for q in range(row):
+        solver = place_queen(q, column, solver)
+    return solver
+
+
+def place_queen(q, column, prev_solver):
+    solver_queen = []
+    for array in prev_solver:
+        for x in range(column):
+            if is_safe(q, x, array):
+                solver_queen.append(array + [x])
+    return solver_queen
+
+
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
+
+
+if __name__ == '__main__':
+
+    N = sys.argv[1]
+    if type(N) is not int:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    solver = solve(N, N)
+    for array in solver:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,1 @@
+This repo contains the answer file for the interview question of N queens problem
