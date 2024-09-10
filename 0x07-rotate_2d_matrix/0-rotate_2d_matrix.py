07b3c94421e208371cc5ee3e9666c92eda5f1163
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-11 00:01:26 +0300

    task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,9 +8,20 @@ def rotate_2d_matrix(matrix: List[List]) -> None:
     '''In-place 2D-square matrix rotation function
     that performs a 90 degree clock-wise rotation'''
 
-    for i in range(len(matrix)):
-        for j in range(i, len(matrix)):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    left, right = 0, len(matrix) - 1
 
-    for row in matrix:
-        row.reverse()
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+
+            topLeft = matrix[top][left + i]
+
+            matrix[top][left + i] = matrix[bottom - i][left]
+
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+
+            matrix[bottom][right - i] = matrix[top + i][right]
+
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
