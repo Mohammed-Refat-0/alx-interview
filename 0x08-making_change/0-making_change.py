5c953433b339956c014d46acee0f33d1489cacab
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-20 05:54:35 +0300

    ans

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''making change problem module'''
+
+
+def makeChange(coins, total):
+    '''determine the fewest number of coins
+    needed to meet a given amount total, otherwise -1 if not possible
+    '''
+    if total <= 0:
+        return 0
+
+    rem = total
+    count = 0
+    index = 0
+    sorted_coins = coins.sort(reverse=True)
+
+    while rem > 0:
+        if index >= len(coins):
+            return -1
+        if rem - sorted_coins[index] >= 0:
+            rem -= sorted_coins[index]
+            count += 1
+        else:
+            index += 1
+    return count
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+This repo contains the ans file for the interview question of 'Making Change'
